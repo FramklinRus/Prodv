9a9c0a7161eccf64cef2901e22959fa0aa5cf52d
Ruslan B <super.konor2015@mail.ru>
2025-07-01 10:02:49 +0500

    Переделал задания

--- 2/2/2.2.cpp
+++ 2/2/2.2.cpp
@@ -3,13 +3,13 @@
 class smart_array
 {
     int size;
-    int* a;
+    int* array;
     int count;
 public:
     smart_array(int c)
     {
         size = c;
-        a = new int[size];
+        array = new int[size];
 
         count = 0;
     }
@@ -18,33 +18,45 @@ public:
     {
 
         if (count > (size - 1)) {
-            std::cout << "Full arr\n";
+            throw std::out_of_range("Index out of range");
         }
         else {
-            a[count] = add;
+            array[count] = add;
             count++;
         }
 
     }
     int get_element(int take)
     {
-        if (take > (size - 1) || take < 0) { throw std::out_of_range("Index out of range"); }
+        if (take > (size - 1) || take < 0) { throw std::out_of_range("Index out of range");  }
         else {
-            return a[take];
+            return array[take];
         }
 
+    }
+    void creat_new_array(const smart_array& smart_array)
+    {
+        delete[] array;
+        size = smart_array.size;
+        count = smart_array.count;
+        array = new int[size];
+        for (int i = 0; i < size;i++)
+        {
+            array[i] = smart_array.array[i];
+        }
     }
     smart_array& operator=(const smart_array& smart_array)
     {
         if (&smart_array != this)
         {
-            *a = *smart_array.a;
+                     creat_new_array(smart_array);
         }
-        return *this;
+       
+                return *this;
     }
     ~smart_array()
     {
-        delete[] a;
+        delete[] array;
     }
 };
 
@@ -56,10 +68,11 @@ int main()
     arr.add_element(1);
     arr.add_element(4);
     arr.add_element(155);
-
+   
     smart_array new_array(2);
     new_array.add_element(44);
     new_array.add_element(34);
-
-    arr = new_array;
+    
+   arr = new_array;
+ 
 }
